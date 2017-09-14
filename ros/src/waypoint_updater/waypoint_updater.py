5996225571c4486dd2ab6fa4595e88b12a7eb0b7
shayko18 <shaykoren18@gmail.com>
2017-09-14 15:37:05 +0300

    First version for handling traffic lights

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -2,7 +2,7 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped
-from styx_msgs.msg import Lane, Waypoint
+from styx_msgs.msg import Lane, Waypoint, TrafficLight, TrafficLightArray
 
 import math
 
@@ -22,6 +22,8 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+MAX_SPEED = 10.0
+MIN_SPEED = 1.0
 
 
 class WaypointUpdater(object):
@@ -30,35 +32,104 @@ class WaypointUpdater(object):
 
         self.current_pose = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb, queue_size=1)
         self.base_waypoints = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb, queue_size=1)
+        self.traffic_waypoint = rospy.Subscriber('/traffic_waypoint', TrafficLightArray, self.traffic_cb, queue_size=1)
 
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
+        # TODO: Add a subscriber for /obstacle_waypoint below
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # List of route waypoints
         self.map_wp = []
+        
+        # velocity map
+        self.velocity_map = []
+        
+        # If see a red traffic light ahead 
+        self.stop_wp_active = False
+        
+        # current car position
+        self.car_position = None
+        
+        # next wp index
+        self.next_wp_idx = None
 
         rospy.spin()
 
     def pose_cb(self, msg):
+        self.car_position = msg
+        
         nearest_wp = self.find_nearest_wp(msg.pose.position.x, msg.pose.position.y)
         if nearest_wp < 0:
             return
-
+        self.next_wp_idx = nearest_wp+1
+            
         # Pub data
         lane = Lane()
         lane.header.frame_id = msg.header.frame_id
         lane.header.stamp = rospy.get_rostime()
         lane.waypoints = self.map_wp[nearest_wp:nearest_wp+LOOKAHEAD_WPS]
+        
+        n_stop_wp = len(self.velocity_map)
+        for i in range(LOOKAHEAD_WPS):
+            if (self.stop_wp_active==False):
+                vel_2_use = MAX_SPEED
+            else:
+                if (i<n_stop_wp):
+                    vel_2_use = self.velocity_map
+                else:
+                    vel_2_use = 0.0
+            set_waypoint_velocity(lane.waypoints, lane.waypoints[i], vel_2_use)
+        
         self.final_waypoints_pub.publish(lane)
+        
 
     def waypoints_cb(self, waypoints):
-        self.map_wp = waypoints.waypoints;
+        self.map_wp = waypoints.waypoints
 
+        
+        
+    def calc_stop_wp_map(self, tl): # set the velocities when we see a red light ahead
+        tl_wp_idx = self.find_nearest_wp(tl.pose.position.x, tl.pose.position.y)
+        if (tl_wp_idx < 0) or (tl_wp_idx < self.next_wp_idx):
+            return False
+        
+        n_wp = tl_wp_idx - self.next_wp_idx                                     # number of wp 
+        vel = self.get_waypoint_velocity(self.map_wp[self.next_wp_idx]);
+        if (vel < MIN_SPEED):
+            vel = MIN_SPEED
+        
+        dv = vel / n_wp   # by how much to reduce the speed
+        
+        self.velocity_map = []
+        for i in range(n_wp):
+            v_next = v_next - dv
+            if (v_next < 0.0):
+                v_next = 0.0
+            self.velocity_map.append(v_next)
+            
+        return True   
+        
+        
+        
     def traffic_cb(self, msg):
-        # TODO: Callback for /traffic_waypoint message. Implement
-        pass
+        tl_vec = msg.tl_vec   # traffic lights that we identified
+
+        tl_ahead = None
+        d_min = 1000.0
+        for tl in tl_vec:     # find the traffic light ahead of us
+            d_curr = self.distance_sqr(self.car_position.pose.position.x, self.car_position.pose.position.y, tl.pose.pose.position.x, tl.pose.pose.position.y)
+            next_car_position = self.map_wp[self.next_wp_idx]
+            d_next = self.distance_sqr(self.next_car_position.pose.position.x, self.next_car_position.pose.position.y, tl.pose.pose.position.x, tl.pose.pose.position.y)
+            if (d_curr < d_min) and (d_next < d_curr):   # we make sure the traffic light is getting closer
+                d_min = d_curr
+                tl_ahead = tl
+        
+        if (tl_ahead and (tl_ahead.state == 0)):   # red light ahead
+            succ = self.calc_stop_wp_map(tl_ahead)
+            self.stop_wp_active = succ
+        else:
+            self.stop_wp_active = False
+        
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
