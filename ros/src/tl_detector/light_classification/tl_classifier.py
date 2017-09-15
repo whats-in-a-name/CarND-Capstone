8442f4fbb0bd476c09fac2b89f42de158760c66c
Lucas Souza <lucasosouza@gmail.com>
2017-09-15 19:58:32 -0300

    initial version - traffic light classification

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,19 +1,63 @@
+import numpy as np
+import os
+
+# classifier
+import cv2
+from keras.models import load_model
+from keras import backend as K
+
+
+# ROS
 from styx_msgs.msg import TrafficLight
 
+
 class TLClassifier(object):
+
     def __init__(self):
-        #TODO load classifier
-        pass
+        """ Initialize model and load weights """
 
-    def get_classification(self, image):
-        """Determines the color of the traffic light in the image
+        # set backgroun as theano
+        # tensorflow conflicting with webservice
+        backend = "theano"
+        if K.backend() != backend:
+            os.environ['KERAS_BACKEND'] = backend
+            reload(K)
+            assert K.backend() == backend
 
-        Args:
-            image (cv::Mat): image containing the traffic light
+        root_lib = os.path.dirname(os.path.realpath(__file__))
+        data_path = root_lib + '/model_data/'
+        print("Data path: {}".format(data_path))
 
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+        self.model = load_model(data_path + 'tl_classifier_py2.h5')
+        self.model._make_predict_function()
 
-        """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        self.encoder = {
+            0: TrafficLight.RED,
+            1: TrafficLight.YELLOW,
+            2: TrafficLight.GREEN
+        }
+
+
+    def get_classification(self,image):  
+        """ Predict using pre-trained model """
+
+        # crop - remove car
+        image = image[0:770, :]
+
+        # resize
+        height = 50
+        width = 88
+        image = cv2.resize(image, (width,height))
+
+        # scale and center
+        image = (image/255) -.5
+
+        # reshape as array
+        image = image.reshape((1,height,width,3))
+
+        # predict (array as input)
+        pred = self.model.predict(image)
+        klass = int(round((pred[0][0])))
+
+        return self.encoder[klass]
+
