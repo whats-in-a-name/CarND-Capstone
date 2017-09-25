87cf451399441dd14e656e6f048b6bd5aea14352
Ian Zhang <ianzhang1986@gmail.com>
2017-09-25 06:20:06 -0700

    feature: Reduce pure pursuit queue size and freq
    Signed-off-by: Ian Zhang <ianboyanzhang@gmail.com>

--- ros/src/waypoint_follower/src/pure_pursuit.cpp
+++ ros/src/waypoint_follower/src/pure_pursuit.cpp
@@ -30,8 +30,11 @@
 
 #include "pure_pursuit_core.h"
 
-constexpr int LOOP_RATE = 30; //processing frequency
+// Setup for low performance system
+// Reduce LOOP_RATE to be same as dbw loop rate
+constexpr int LOOP_RATE = 20; //processing frequency
 
+const int PUB_SUB_QUEUE_SIZE = 1;
 
 int main(int argc, char **argv)
 {
@@ -56,11 +59,14 @@ int main(int argc, char **argv)
   ROS_INFO("set subscriber...");
   // subscribe topic
   ros::Subscriber waypoint_subscriber =
-      nh.subscribe("final_waypoints", 10, &waypoint_follower::PurePursuit::callbackFromWayPoints, &pp);
+//      nh.subscribe("final_waypoints", 10, &waypoint_follower::PurePursuit::callbackFromWayPoints, &pp);
+      nh.subscribe("final_waypoints", PUB_SUB_QUEUE_SIZE, &waypoint_follower::PurePursuit::callbackFromWayPoints, &pp);
   ros::Subscriber ndt_subscriber =
-      nh.subscribe("current_pose", 10, &waypoint_follower::PurePursuit::callbackFromCurrentPose, &pp);
+//      nh.subscribe("current_pose", 10, &waypoint_follower::PurePursuit::callbackFromCurrentPose, &pp);
+      nh.subscribe("current_pose", PUB_SUB_QUEUE_SIZE, &waypoint_follower::PurePursuit::callbackFromCurrentPose, &pp);
   ros::Subscriber est_twist_subscriber =
-      nh.subscribe("current_velocity", 10, &waypoint_follower::PurePursuit::callbackFromCurrentVelocity, &pp);
+//      nh.subscribe("current_velocity", 10, &waypoint_follower::PurePursuit::callbackFromCurrentVelocity, &pp);
+      nh.subscribe("current_velocity", PUB_SUB_QUEUE_SIZE, &waypoint_follower::PurePursuit::callbackFromCurrentVelocity, &pp);
 
   ROS_INFO("pure pursuit start");
   ros::Rate loop_rate(LOOP_RATE);
