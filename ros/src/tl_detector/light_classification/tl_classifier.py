9871a68d0764061d65e40c69e81a9a40d2a4807b
Lucas Souza <lucasosouza@gmail.com>
2017-09-29 19:38:15 +0200

    changing from Theano to TF

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -5,6 +5,7 @@ import cv2
 from keras.models import load_model
 from keras import backend as K
 import time
+import tensorflow as tf
 
 from math import ceil, floor
 
@@ -18,19 +19,12 @@ class TLClassifier(object):
     def __init__(self):
         """ Initialize model and load weights """
 
-        # set backgroun as theano
-        # tensorflow conflicting with webservice
-        backend = "theano"
-        if K.backend() != backend:
-            os.environ['KERAS_BACKEND'] = backend
-            reload(K)
-            assert K.backend() == backend
-
         root_lib = os.path.dirname(os.path.realpath(__file__))
         data_path = root_lib + '/model_data/'
         print("Data path: {}".format(data_path))
 
         self.model = load_model(data_path + 'tl_classifier_py2f.h5')
+        self.graph = tf.get_default_graph()
         self.model._make_predict_function()
 
         self.encoder = {
@@ -43,23 +37,17 @@ class TLClassifier(object):
     def get_classification(self,image):  
         """ Predict using pre-trained model """
 
-        # crop
-        image = image
-
-        # resize
-        height = 90
-        width = 40
-        # image = cv2.resize(image, (width,height))
-
         # scale and center
         image = (image/255) -.5
 
         # reshape as array
+        height = 90
+        width = 40
         image = image.reshape((1,height,width,3))
 
-        # predict (array as input)
-        pred = self.model.predict(image)
-        klass = np.argmax(pred)
+        with self.graph.as_default():
+            pred = self.model.predict(image)
+            klass = np.argmax(pred)
 
         tl_state = self.encoder[klass]
         # print("Got class: {}, {}".format(tl_state, time.time()))
