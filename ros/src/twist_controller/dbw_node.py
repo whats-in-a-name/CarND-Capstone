16217543178b9d65db50b2b60778d37999e0db54
Ian Zhang <ianzhang1986@gmail.com>
2017-10-13 01:46:49 -0700

    feature: Revert brake control to torque
    Signed-off-by: Ian Zhang <ianboyanzhang@gmail.com>

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -92,7 +92,7 @@ class DBWNode(object):
         self.throttle = -brake
         bcmd = BrakeCmd()
         bcmd.enable = True
-        bcmd.pedal_cmd_type = BrakeCmd.CMD_PERCENT
+        bcmd.pedal_cmd_type = BrakeCmd.CMD_TORQUE
         bcmd.pedal_cmd = brake
         self.brake_pub.publish(bcmd)
 
